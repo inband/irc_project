d1dd87455a7c1fe685174a607fb7ae310ab0a432
inband <nat@inband.io>
2017-10-25 11:16:51 +0000

    added recv

--- irc_server.c
+++ irc_server.c
@@ -13,10 +13,13 @@
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-
+#include <unistd.h> // unsigned int sleep(unsigned int seconds);
+#include <string.h> // size_t strlen(const char *s);
+#include <ctype.h> // int isprint(int c);
 
 #define LOGFILE stderr
-
+#define MESSAGE_BUF 513	// one more than limit to add '\0'
+#define MESSAGE_BUF_LIMIT 512 
 
 
 
@@ -77,9 +80,64 @@ int main(int argc, char *argv[])
 	if ((client_sock_fd = accept(serv_sock_fd, (struct sockaddr *)&client_addr, (socklen_t *) &client_length)) == -1)
 		fprintf(LOGFILE, "%s\n", "Error: Unable to accept\n");
 
+	// welcome
+	// ssize_t send(int sockfd, const void *buf, size_t len, int flags);
+	// On success, these calls return the number of bytes sent.  On error, -1 is returned, and errno is set appropriately.
+	char *welcome_message = "Welcome\n";
+	ssize_t bytes_sent = send(client_sock_fd, welcome_message, strlen(welcome_message), 0);
+	printf("%zd Bytes sent\n", bytes_sent);
 
 
+	//specification states that messages are no longer than 512 bytes
+	// buffer will be 513 to append '\0' if message is well formated and exactly 512 bytes
+	char recv_buf[MESSAGE_BUF];
+	
+	while (1)
+	{
+		// Using telnet to connect to irc_server 6667 to test.  Telnet automagically sends \r\n
+		// receive message
+		// ssize_t recv(int sockfd, void *buf, size_t len, int flags);	
+		if((ssize_t bytes_recv = recv(client_sock_fd, recv_buf, MESSAGE_BUF_LIMIT, 0)) == MESSAGE_BUF_LIMIT)
+		{
+		// can buffer be checked to see if it was exceeded
+			if (bytes_recv[MESSAGE_BUF_LIMIT] == '\n' && bytes_recv[MESSAGE_BUF_LIMIT - 1] == '\r')
+			{	
+				recv_buf[(int)bytes_recv] = '\0';  //Now message is a string
+				//handle message
+			}
+			else
+			{
+			// disgard	
+			//send message "too big!"
+			}	
+		}
+		else
+		{
+			recv_buf[(int)bytes_recv] = '\0';  //Now message is a string
+			printf("%zd %s", bytes_recv,  "Bytes received\n");
+			//printf("%s", recv_buf);
+			for (int i = 0; recv_buf[i] != '\0'; i++)
+			{
+				if (isprint(recv_buf[i]))
+					printf("%c", recv_buf[i]);
+				else if (recv_buf[i] == '\n')
+					printf("\\%x\n", recv_buf[i]);
+				else
+					printf("\\%x", recv_buf[i]);
+			}
 
+		}
+		// check message
+
+		
+		// execute message
+
+
+		// respond to message
+
+
+	}
+		
 
 
 	return 0;
